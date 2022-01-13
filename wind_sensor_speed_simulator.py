60746e097073b19daaea30c302adca9fea318b80
Бен Аладжем <91260379+BenAladjem@users.noreply.github.com>
2022-01-13 16:55:54 +0200

    Add files via upload

--- wind_sensor_speed_simulator.py
+++ wind_sensor_speed_simulator.py
@@ -9,35 +9,39 @@ p14 = machine.Pin(14, machine.Pin.OUT)
 p14.value(1)
 
 #Увеличава  плавно честотата на импулсите в зададени граници на скоростта
-# min speed = 2.4 km/h = 1 imp/s - value(0) = 1/3 sec = 333ms, value (1) = 2/3sec = 666ms
+# min speed = 2.4 km/h = 1 imp/s - value(0) = 1/4 sec = 250ms, value (1) = 3/4sec = 750ms
 # max speed = 240km/h
 # coefficient = 2.4 / speed
 
 def impulse_generator(speed):
-    coeff = 2.4/speed
-    while true:
-        one_imp(coeff)
-        time.sleep(coeff / 60)
+    coeff = speed / 2.4
+    dur = 1000 / coeff 
+    i = 10000 / dur
+    while i >= 0:
+        one_imp(dur)
+        time.sleep_ms(dur * 0.75)
+        i -= 1
 
-def one_imp(coefficient) #speed
+def one_imp(coefficient):
+    print('*')
     global p14
     p14.value(0)
-    time.sleep(coefficient / 30)
+    time.sleep_ms(coefficient * 0.25)
     p14.value(1)
-    #time.sleep(0.0666 * coefficient)
+    #time.sleep(0.0750 * coefficient)
 
 
 
 
 time_int = 240
-speed = 1 # km/h
+speed = 5 # km/h
 count = 0
 
 step = 5
 coefficient = 2.4 / speed
 while speed <= 300:
     impulse_generator(speed)
-    time.sleep(5)
+    #time.sleep(5)
     speed += step
 
 
